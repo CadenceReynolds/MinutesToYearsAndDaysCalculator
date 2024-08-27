886cbce4550df606ba5484744c58f1ecfe21ff9e
CadenceReynolds <reynocal001@warren.k12.in.us>
2024-08-27 13:08:13 -0400

    done3

--- src/minutesToYearsAndDaysCalculator.java
+++ src/minutesToYearsAndDaysCalculator.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class minutesToYearsAndDaysCalculator {
+public class minutesToYearsAndDaysCalculator {
+    public static void main(String[] args) {
+        printYearsAndDays(525600);
+        printYearsAndDays(1051200);
+        printYearsAndDays(561600);
+    }
+
+    public static void printYearsAndDays(long mins){
+        if(mins < 0){
+            System.out.println("Invalid Value");
+        }
+            long hours = mins / 60;
+            long days = hours / 24;
+            long years = days / 365;
+            long extraDays = days % 365;
+        System.out.println(mins + "min = " + years + "y and " + extraDays + "d");
+    }
 }
